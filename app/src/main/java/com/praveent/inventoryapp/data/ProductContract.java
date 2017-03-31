17b040b4397810e243d73799d9324ae1a139c0c8
PRAVEEN <praveen@pixlmatrix.com>
2017-03-31 20:54:58 +0530

    Initial commit

---
+++ app/src/main/java/com/praveent/inventoryapp/data/ProductContract.java
@@ -0,0 +1,38 @@
+package com.praveent.inventoryapp.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Praveen on 31/03/2017.
+ */
+
+public class ProductContract {
+    public static final String CONTENT_AUTHORITY = "com.praveent.inventoryapp";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PRODUCTS = "products";
+
+    private ProductContract() {
+
+    }
+
+    public static final class ProductEntry implements BaseColumns {
+
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
+
+        public final static String TABLE_NAME = "products";
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_PRODUCT_NAME ="name";
+        public final static String COLUMN_PRODUCT_QUANTITY = "quantity";
+        public final static String COLUMN_PRODUCT_PRICE = "price";
+        public final static String COLUMN_PRODUCT_IMAGE = "image";
+    }
+
+}
